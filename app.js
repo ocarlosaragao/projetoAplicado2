67b4900acca74e67caf8232828db2fff80bf2e8a
aragao <acrafilho@gmail.com.br>
2024-09-16 22:47:28 -0300

    Remove files and update project

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// app.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, onChildAdded } from 'firebase/database';
 
 // Firebase configuration (replace with your actual config)
 const firebaseConfig = {
-    apiKey: "AIzaSyCeuGOn0QV23KBLA9Z3XFso2mNcNjbK6tY",
-    authDomain: "meeting-scheduler-ca23e.firebaseapp.com",
-    projectId: "meeting-scheduler-ca23e",
-    storageBucket: "meeting-scheduler-ca23e.appspot.com",
-    messagingSenderId: "994508481089",
-    appId: "1:994508481089:web:637d9990464a13901c5cc0"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
